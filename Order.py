82093285e9b4a273e75c68ce2f4d9b2e2914ee96
Mikedev <mdv1994@gmail.com>
2017-01-29 16:07:24 +0100

    Add delivery and add method to Warehouse

--- Order.py
+++ Order.py
@@ -5,5 +5,5 @@ from Product import Product
 
 
 class Order:
-    def __init__(self, deliverPosition: Point, products: List[(Product, int)]):
+    def __init__(self, deliverPosition: Point, products: list[(Product, int)]):
         self.deliverPosition,  self.products, self.satisfied = deliverPosition, products, False
--- Warehouse.py
+++ Warehouse.py
@@ -1,9 +1,11 @@
-from typing import List
+from functools import reduce
 
 from Point import Point
 from Product import Product
 
 
 class Warehouse:
-    def __init__(self, position : Point, products : List[(Product, int)]):
+    def __init__(self, position : Point, products : list[(Product, int)]):
         self.position, self.products = position, products
+    def quantityOf(self, productType : int) -> int:
+        return reduce(lambda q1, q2 : q1 + q2, map(lambda pair: pair[1] ,filter(lambda pair : pair[0].type == productType, self.products)))
